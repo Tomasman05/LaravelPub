ba1eecf0bc61f18ffc0b0caa33a00caed99936c3
Vito <lolozol80xd@gmail.com>
2024-01-25 08:58:41 +0100

    ongoing

---
+++ app/Http/Controllers/pudController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class pudController extends Controller
+{
+    public function getDrinks(){
+        $drinks = DB::table( "drinks" )->get();
+
+        return $drinks;
+    }
+
+    public function getOneDrink($drinkName){
+
+        $drink= DB::table( "drinks" )->where("drink", $drinkName)->first();
+
+        return $drink;
+    }
+
+    public function getLikeDrinks($likeText){
+        $likeText = "%".$likeText."%";
+       $drinks = DB::table("drinks")->where( "drink", "like", $likeText)->get();
+
+       return $drinks;
+    }
+
+    public function getLess30($value1, $value2){ 
+
+        $drinks = DB::table("drinks")->whereBetween( "amount", [$value1, $value2])->get();
+
+        return $drinks;
+    }
+
+    public function addDrink(){
+        $drink = DB::table( "drinks" )->insert([
+            "drink" => "Sprite",
+            "amount" => 54,
+            "type_id" => 4,
+            "package_id" => 1
+        ]);
+
+        return $drink;
+    }
+
+    public function getDrinkWithType(){
+        $drinks = DB::table("drinks")->
+            select(
+                "drinks.drink as Ital",
+                "drinks.amount as Mennyiség",
+                "types.type as Típus"
+            )->join(
+                "types","drinks.type_id","=","types.id"
+            )->get();
+        return $drinks;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pudController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get("/drinks", [ pudController::class, "getDrinks"]);
+
+Route::get("/oneDrink/{drinkName}", [ pudController::class, "getOneDrink"]);
+
+Route::get("/likeDrink/{likeText}", [ pudController::class, "getLikeDrinks"]);
+
+Route::get("/between/{value1}/{value2}", [ pudController::class, "getLess30"]);
+
+Route::get("/adddrink", [ pudController::class, "addDrink"]);
+Route::get("/drinkwtype",[pudController::class, "getDrinkWithType"]);
